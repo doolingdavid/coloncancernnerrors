78b219067011c40c1490b36f233d5f6ee23922ca
David Curtis Dooling <doolingdavid@gmail.com>
2016-02-08 11:22:16 -0700

    changed the number of replicas to 800

--- hello.py
+++ hello.py
@@ -1250,7 +1250,7 @@ def results():
         listofcurves.append(Asurv)
 
 
-        for i in range(1500):
+        for i in range(800):
             import random 
             numberofsubjects = get_month_from_cdf(datacountscdf) + 1
             samp1 = get_one_group(basecdf, numberofsubjects)
